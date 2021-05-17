41ac13c5e0bfaa79f38884cef05c70eafbea1851
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-16 21:50:42 -0500

    Fixed top padding so < Previous App is not intersecting.

--- components/TopPadding.tsx
+++ components/TopPadding.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { View } from "react-native";
 
 export default function TopPadding() {
-  return <View style={{ height: 32, backgroundColor: "#e0e" }} />;
+  return <View style={{ height: 44, backgroundColor: "#e0e" }} />;
 }
