2bf059d57bf34403d52b78b5358502556a5a8647
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-16 23:51:40 -0500

    Added flags as proof of concept.

--- components/LearnLanguage.tsx
+++ components/LearnLanguage.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { View, Text } from "react-native";
+// @ts-ignore
+import Flag from "react-native-flags";
 
 export default function LearnLanguage() {
   return (
     <View
       style={{
         flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Text>AAAA</Text>
+      <div style={{ position: "fixed", zIndex: 10 }}>
+        <Flag size={64} type={"flat"} code={"TR"} />
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          backgroundColor: "white",
+          width: 64,
+          height: 44,
+        }}
+      />
     </View>
   );
 }
--- components/TranslateLanguage.tsx
+++ components/TranslateLanguage.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { View } from "react-native";
+// @ts-ignore
+import Flag from "react-native-flags";
 
 export default function TranslateLanguage() {
   return (
     <View
       style={{
         flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
       }}
-    />
+    >
+      <div style={{ position: "fixed", zIndex: 10 }}>
+        <Flag size={64} type={"flat"} code={"US"} />
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          backgroundColor: "white",
+          width: 64,
+          height: 44,
+        }}
+      />
+    </View>
   );
 }
