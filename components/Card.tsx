f8bf8f23af53d1bd669894762523b192f06d01d1
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-19 13:42:19 -0500

    Flippable cards.

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,29 +1,80 @@
-import React from "react";
-import { View } from "react-native";
+import React, { createRef } from "react";
+import { TouchableOpacity, View } from "react-native";
 import { commonBlue, tongueRed, whiteBackground } from "../styles/AppTheme";
+import CardFlip from "react-native-card-flip";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-export const topPaddingHeight = 44;
-export default function Card(props: { children: React.ReactNode }) {
+export default function Card(props: {
+  firstSide: React.ReactNode;
+  secondSide: React.ReactNode;
+}) {
+  let cardRef: CardFlip | null = null;
   return (
     <View
-      style={{
-        width: "80%",
-        flex: 1,
-        marginTop: "10%",
-        marginBottom: "10%",
-        borderRadius: 10,
-        backgroundColor: whiteBackground,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        shadowOffset: { height: 1, width: 1 },
-        shadowOpacity: 0.8,
-        shadowRadius: 10,
-        shadowColor: "#000",
-      }}
-      elevation={8}
+      style={{ width: "80%", flex: 1, marginTop: "10%", marginBottom: "10%" }}
     >
-      {props.children}
+      <CardFlip
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+        ref={(card: CardFlip | null) => {
+          cardRef = card;
+        }}
+      >
+        <TouchableWithoutFeedback
+          style={{ height: "100%", width: "100%" }}
+          onPress={() => {
+            cardRef?.flip();
+          }}
+        >
+          <View
+            style={{
+              height: "100%",
+              width: "100%",
+              borderRadius: 10,
+              backgroundColor: whiteBackground,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              shadowOffset: { height: 1, width: 1 },
+              shadowOpacity: 0.8,
+              shadowRadius: 10,
+              shadowColor: commonBlue,
+            }}
+            // @ts-ignore
+            elevation={8}
+          >
+            {props.firstSide}
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback
+          style={{ height: "100%", width: "100%" }}
+          onPress={() => {
+            cardRef?.flip();
+          }}
+        >
+          <View
+            style={{
+              height: "100%",
+              width: "100%",
+              borderRadius: 10,
+              backgroundColor: whiteBackground,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              shadowOffset: { height: 1, width: 1 },
+              shadowOpacity: 0.8,
+              shadowRadius: 10,
+              shadowColor: commonBlue,
+            }}
+            // @ts-ignore
+            elevation={8}
+          >
+            {props.secondSide}
+          </View>
+        </TouchableWithoutFeedback>
+      </CardFlip>
     </View>
   );
 }
