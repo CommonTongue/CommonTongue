2b048f516e3c6402b669657c863cea39ac8abb29
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-19 15:53:16 -0500

    refactored language picker.

--- components/LanguagePicker.tsx
+++ components/LanguagePicker.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 import {
   faCheckSquare,
   faChevronDown,
   faChevronUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { useState } from "react";
 import DropDownPicker, { ValueType } from "react-native-dropdown-picker";
 import { commonBlue, tongueRed, whiteBackground } from "../styles/AppTheme";
 
@@ -36,6 +36,7 @@ const supportedLanguages = [
   "Vietnamese",
   "English",
 ].sort();
+
 export default function LanguagePicker(props: {
   onLanguagePick: (value: ValueType) => void;
 }) {
@@ -49,27 +50,13 @@ export default function LanguagePicker(props: {
 
   return (
     <DropDownPicker
-      containerStyle={{
-        /** Styles the outer-most element. */
-        backgroundColor: commonBlue,
-        borderRadius: 3,
-        borderWidth: 0,
-      }}
-      style={{
-        /** Styles the top large element. */
-        backgroundColor: commonBlue,
-        borderWidth: 0,
-      }}
-      textStyle={{
-        color: commonBlue,
-        fontWeight: "bold",
-      }}
-      labelStyle={{
-        /** Styles the element that sticks to the selector when an item is selected. */
-        color: "white",
-        marginLeft: 10,
-      }}
-      placeholderStyle={{ color: whiteBackground }}
+      containerStyle={styles.container}
+      style={styles.dropDown}
+      textStyle={styles.text}
+      labelStyle={styles.label}
+      placeholderStyle={styles.placeholder}
+      searchTextInputStyle={styles.searchTextInput}
+      searchPlaceholderTextColor={tongueRed}
       ArrowDownIconComponent={(props) => {
         return <FontAwesomeIcon color={whiteBackground} icon={faChevronDown} />;
       }}
@@ -79,8 +66,6 @@ export default function LanguagePicker(props: {
       TickIconComponent={(props) => {
         return <FontAwesomeIcon color={commonBlue} icon={faCheckSquare} />;
       }}
-      searchTextInputStyle={{ borderWidth: 0, color: tongueRed }}
-      searchPlaceholderTextColor={tongueRed}
       searchable={true}
       searchPlaceholder={`search for a language...`}
       placeholder={"pick a language"}
@@ -97,3 +82,28 @@ export default function LanguagePicker(props: {
     />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    /** Styles the outer-most element. */
+    backgroundColor: commonBlue,
+    borderRadius: 3,
+    borderWidth: 0,
+  },
+  dropDown: {
+    /** Styles the top large element. */
+    backgroundColor: commonBlue,
+    borderWidth: 0,
+  },
+  text: {
+    color: commonBlue,
+    fontWeight: "bold",
+  },
+  label: {
+    /** Styles the element that sticks to the selector when an item is selected. */
+    color: "white",
+    marginLeft: 10,
+  },
+  placeholder: { color: whiteBackground },
+  searchTextInput: { borderWidth: 0, color: tongueRed },
+});
