38fe2636c4a68426dc072208c8ede5c6b1fdecfa
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-19 16:17:43 -0500

    all components refactored.

--- components/TopPadding.tsx
+++ components/TopPadding.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { tongueRed } from "../styles/AppTheme";
 
 export const topPaddingHeight = 44;
 export default function TopPadding() {
-  return (
-    <View style={{ height: topPaddingHeight, backgroundColor: tongueRed }} />
-  );
+  return <View style={styles.topPadding} />;
 }
+
+const styles = StyleSheet.create({
+  topPadding: { height: topPaddingHeight, backgroundColor: tongueRed },
+});
