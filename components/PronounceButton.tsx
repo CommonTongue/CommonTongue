59cf0a3542d998f8ef50ccd2c721a9027af28074
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-19 14:23:55 -0500

    Styled pronounce button to be white on press.

--- components/PronounceButton.tsx
+++ components/PronounceButton.tsx
@@ -2,11 +2,11 @@ import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import React, { useState } from "react";
 import { TouchableWithoutFeedback } from "react-native";
-import { commonBlue, tongueRed } from "../styles/AppTheme";
+import { commonBlue, tongueRed, whiteBackground } from "../styles/AppTheme";
 
 export default function PronounceButton() {
   const [talking, setTalking] = useState(false);
-  const volumeColor = talking ? commonBlue : "#000";
+  const volumeColor = talking ? whiteBackground : "#000";
   const pronounceWord = () => {
     setTalking(true);
     setTimeout(() => {
